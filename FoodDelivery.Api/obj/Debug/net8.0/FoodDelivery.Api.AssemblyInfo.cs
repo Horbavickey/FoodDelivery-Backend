8491f8576413dd2abbe3173eb4a2b2d1a4b160f5
horbavickey <obanlajesupemi2021@gmail.com>
2025-09-30 02:17:09 +0700

    Initialize solution and projects (Api, APplication, Domain, Infrastructure)

--- FoodDelivery.Api/obj/Debug/net8.0/FoodDelivery.Api.AssemblyInfo.cs
+++ FoodDelivery.Api/obj/Debug/net8.0/FoodDelivery.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodDelivery.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69175e12ae4df8724faaf263f8af548793583291")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodDelivery.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodDelivery.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
